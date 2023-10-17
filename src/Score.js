0152f80c76f5068a1175a41e3c8e9ac9a7560ebe
Fuad Ismail <fuad1502@gmail.com>
2023-10-17 21:26:59 +0700

    Refactored and reformatted Score component

--- src/Score.js
+++ src/Score.js
@@ -1,26 +1,26 @@
-export default function Score ({ gameStatus, timer, wordScores }) {
-  if (gameStatus == "ENDED") {
-    const wpm = calculateWPM(wordScores, timer);
-    return <div className="score">Average WPM: {wpm}</div>;
-  } else {
-    return <div className="score">Average WPM: -</div>;
-  }
+export default function Score({ gameStatus, timer, wordScores }) {
+  const wpm = calculateWPM(wordScores, timer);
+  return (
+    <div className="score">
+      Average WPM: {gameStatus == "ENDED" ? wpm : "-"}
+    </div>
+  );
 }
 
 function calculateWPM(wordScores, timer) {
   let correctWords = 0;
-  for(let i = 0; i < wordScores.length - 1; i++) {
+  for (let i = 0; i < wordScores.length - 1; i++) {
     let fail = false;
     const wordScore = wordScores[i];
-    for(let j = 0; j < wordScore.length; j++) {
-      if(wordScore[j] != 0) {
+    for (let j = 0; j < wordScore.length; j++) {
+      if (wordScore[j] != 0) {
         fail = true;
         break;
       }
     }
-    if(!fail) {
+    if (!fail) {
       correctWords++;
     }
   }
-  return correctWords / (timer/60);
+  return correctWords / (timer / 60);
 }
